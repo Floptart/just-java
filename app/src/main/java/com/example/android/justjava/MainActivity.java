7b4246c73b9e7a1b016dce09d945ea2bf478d6c8
Floptart <floptart@gmail.com>
2017-10-09 15:05:09 -0400

    add + and - buttons to quantity section. Add logic to both. Seperate logic for order button and quantities.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -12,6 +12,7 @@ import java.text.NumberFormat;
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +24,7 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
-        displayPrice(2*5);
+        displayPrice(quantity * 5);
     }
 
     /**
@@ -41,5 +41,19 @@ public class MainActivity extends AppCompatActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    public void increment(View view) {
+        quantity = quantity + 1;
+        display(quantity);;
+    }
+
+    public void decrement(View view) {
+        if(quantity == 0){
+            display(0);
+        } else {
+            quantity = quantity - 1;
+            display(quantity);
+        }
     }
 }
